5d7589eb4078c6818a44e91d2da20385836ad96b
Yury Kinchin <yury.kinchyn@gmail.com>
2023-11-29 15:25:13 +0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+    screens: {
+      xs: '480px',
+      sm: '768px',
+      md: '1060px'
+    }
+  },
+  plugins: []
+};
