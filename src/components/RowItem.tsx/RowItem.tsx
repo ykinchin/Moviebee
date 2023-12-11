d60de4ffbc12e3f30e0e677776c4c92b11eba801
Yury Kinchin <yury.kinchyn@gmail.com>
2023-12-11 22:12:09 +0300

    poster size fixed

--- src/components/RowItem.tsx/RowItem.tsx
+++ src/components/RowItem.tsx/RowItem.tsx
@@ -8,12 +8,13 @@ interface RowItemProps {
 }
 const RowItem: FC<RowItemProps> = ({ movie }) => {
   return (
-    <div className='inline-block cursor-pointer relative'>
+    <div className=' cursor-pointer relative flex items-center justify-center'>
       <img
-        className='w-full h-auto block'
+        className='w-full h-auto block max-h-[155px]  object-contain'
         src={`${imageUrl}${movie?.backdrop_path || movie?.poster_path}`}
         alt={movie?.title}
       />
+
       <div className='absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white'>
         <p className='white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center'>
           {movie?.title}
