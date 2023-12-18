4003bae2a3dbddc1b939e28eec90b0435202ac83
Yury Kinchin <yury.kinchyn@gmail.com>
2023-12-18 23:32:42 +0300

    movie search feature added

--- src/components/SearchField/SearchField.tsx
+++ src/components/SearchField/SearchField.tsx
@@ -1,15 +1,30 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import SearchIcon from '@mui/icons-material/Search';
 import { Box, IconButton, InputBase, Slide } from '@mui/material';
 import { useQuery } from 'react-query';
 import apiService from '../../api/apiService';
+import useDebounce from '../../hooks/useDebounce';
 
 const SearchInput = () => {
   const [isOpened, setIsOpened] = useState(false);
+  const [searchValue, setSearchValue] = useState('');
 
-  const { data } = useQuery(['tvSearch'], () => apiService.search('tv', ''), {
-    select: ({ data }) => data.results
-  });
+  const debouncedSearchTerm = useDebounce(searchValue, 200);
+
+  const { data } = useQuery(
+    ['movieSearch', debouncedSearchTerm],
+    async () => {
+      if (debouncedSearchTerm) {
+        const response = await apiService.search('movie', debouncedSearchTerm);
+        return response.data;
+      }
+    },
+    {
+      enabled: !!debouncedSearchTerm,
+      select: (data) => data?.results
+    }
+  );
+
   console.log(data);
 
   const handleSearchIconClick = () => {
@@ -33,6 +48,8 @@ const SearchInput = () => {
         <InputBase
           sx={{ borderBottom: '1px solid white', color: 'white' }}
           fullWidth
+          value={searchValue}
+          onChange={(e) => setSearchValue(e.target.value)}
         />
       </Slide>
     </Box>
