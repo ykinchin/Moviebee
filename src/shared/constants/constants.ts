7d3ab969d64626e164830edb8196eb2d1d6d349b
Yury Kinchin <yury.kinchyn@gmail.com>
2023-12-19 22:00:42 +0300

    added constant path to image 100 size

--- src/shared/constants/constants.ts
+++ src/shared/constants/constants.ts
@@ -1,6 +1,7 @@
 export const tmdb_key = import.meta.env.VITE_APP_TMDB_KEY;
 export const guest_login = import.meta.env.VITE_APP_GUEST_LOGIN;
 export const guest_password = import.meta.env.VITE_APP_GUEST_PASSWORD;
+export const image100Url = 'https://image.tmdb.org/t/p/w200';
 export const imageUrl = 'https://image.tmdb.org/t/p/w500';
 export const bannerUrl = 'https://image.tmdb.org/t/p/original';
 export const baseUrl = 'https://api.themoviedb.org/3';
