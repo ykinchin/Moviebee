21ea3bb35d7d28cd6992d9efbeda7e1066e213e6
Yury Kinchin <yury.kinchyn@gmail.com>
2023-12-20 16:25:12 +0300

    style: add style for raiting, not found images

--- src/components/SearchResults/ResultItem.tsx
+++ src/components/SearchResults/ResultItem.tsx
@@ -1,8 +1,9 @@
 import { FC } from 'react';
 import { IMovie } from '../../shared/types';
-import { Card, CardContent, CardMedia, Typography } from '@mui/material';
+import { Card, CardContent, CardMedia, Typography, Box } from '@mui/material';
 import { image100Url } from '../../shared/constants/constants';
 import { useNavigate } from 'react-router-dom';
+import ImageNotSupportedIcon from '@mui/icons-material/ImageNotSupported';
 
 interface ResultItemProps {
   movie: IMovie;
@@ -20,14 +21,44 @@ const ResultItem: FC<ResultItemProps> = ({ movie }) => {
       }}
       onClick={() => navigate('/')}
     >
-      <CardMedia
-        sx={{ width: '80px' }}
-        image={`${image100Url}${movie?.poster_path}`}
-        component='img'
-      />
-      <CardContent>
-        <Typography variant='h6'>{movie.title}</Typography>
-        <Typography component='div'>{movie.vote_average.toFixed(1)}</Typography>
+      {movie.poster_path ? (
+        <CardMedia
+          sx={{ width: '80px' }}
+          image={`${image100Url}${movie.poster_path}`}
+          component='img'
+        />
+      ) : (
+        <Box
+          sx={{
+            height: '130px',
+            width: '110px',
+            backgroundColor: 'gray',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center'
+          }}
+        >
+          <ImageNotSupportedIcon />
+        </Box>
+      )}
+      <CardContent sx={{ width: '100%', maxWidth: '400px' }}>
+        <Typography
+          variant='h6'
+          sx={{ wordBreak: 'break-word', minWidth: '300px' }}
+        >
+          {movie.title}
+        </Typography>
+        <Typography
+          component='div'
+          sx={{
+            border: `1px solid ${movie.vote_average >= 5 ? 'green' : 'red'}`,
+            width: 'max-content',
+            borderRadius: '50%',
+            p: 0.5
+          }}
+        >
+          {movie.vote_average.toFixed(1)}
+        </Typography>
       </CardContent>
     </Card>
   );
