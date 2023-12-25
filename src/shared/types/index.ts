7279bc40ce105e0c6a73040d2bfc76f79a978de8
Yury Kinchin <yury.kinchyn@gmail.com>
2023-12-25 19:00:32 +0300

    feature: add type for credits data

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -67,3 +67,37 @@ export interface IDetailedMovie extends IMovie {
   status: string;
   tagline: string;
 }
+
+export interface ICast {
+  adult: boolean;
+  cast_id: number;
+  character: string;
+  credit_id: string;
+  gender: number;
+  id: number;
+  known_for_department: string;
+  name: string;
+  order: number;
+  original_name: string;
+  popularity: number;
+  profile_path: string;
+}
+
+export interface ICrew {
+  adult: boolean;
+  credit_id: string;
+  department: string;
+  gender: number;
+  id: number;
+  job: string;
+  known_for_department: string;
+  name: string;
+  original_name: string;
+  popularity: number;
+  profile_path: string;
+}
+
+export interface ICredits {
+  cast: ICast[];
+  crew: ICrew[];
+}
