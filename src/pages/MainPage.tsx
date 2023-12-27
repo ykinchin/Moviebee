c2a44b20089e7f2a2af45cacb16a322146d956a3
Yury Kinchin <yury.kinchyn@gmail.com>
2023-12-27 22:30:17 +0300

    style: add box padding x

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -2,6 +2,7 @@ import Row from '../components/Row/Row';
 import HeroSection from '../components/HeroSection/HeroSection';
 import { useTvShows } from '../hooks/useTvShows';
 import { useMovie } from '../hooks/useMovies';
+import { Box } from '@mui/material';
 
 const MainPage = () => {
   const { data: popularTvs } = useTvShows('popular');
@@ -12,18 +13,20 @@ const MainPage = () => {
   return (
     <>
       <HeroSection data={popularMovies} />
-      <Row
-        title='Upcoming movies'
-        data={upcomingMovies}
-      />
-      <Row
-        title='On the air TV-Shows'
-        data={onTheAirTvs}
-      />
-      <Row
-        title='Popular TV-Shows'
-        data={popularTvs}
-      />
+      <Box px={15}>
+        <Row
+          title='Upcoming movies'
+          data={upcomingMovies}
+        />
+        <Row
+          title='On the air TV-Shows'
+          data={onTheAirTvs}
+        />
+        <Row
+          title='Popular TV-Shows'
+          data={popularTvs}
+        />
+      </Box>
     </>
   );
 };
