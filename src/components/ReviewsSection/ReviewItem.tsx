c030b27dc850471281bf84053f63196df1b4dce8
Yury Kinchin <yury.kinchyn@gmail.com>
2023-12-30 23:18:20 +0300

    feat: create component for displayng single review

---
+++ src/components/ReviewsSection/ReviewItem.tsx
@@ -0,0 +1,57 @@
+import { FC } from 'react';
+import { IReview } from '../../shared/types';
+import {
+  Card,
+  CardHeader,
+  Avatar,
+  CardContent,
+  Typography
+} from '@mui/material';
+import { imageUrl } from '../../shared/constants/constants';
+
+interface ReviewItemProps {
+  review: IReview;
+}
+const ReviewItem: FC<ReviewItemProps> = ({ review }) => {
+  const dateObject = new Date(review.created_at);
+  const formattedDateTime = dateObject.toLocaleString();
+
+  return (
+    <Card
+      variant='outlined'
+      sx={{
+        backgroundColor: 'inherit',
+        color: 'white',
+        borderColor: 'rgba(255,255,255,0.4)',
+        borderRadius: '0'
+      }}
+    >
+      <CardHeader
+        title={review.author_details.username || review.author}
+        subheader={formattedDateTime}
+        avatar={
+          <Avatar
+            variant='square'
+            src={`${imageUrl}${review.author_details.avatar_path}`}
+            sx={{ width: '80px', height: '100px' }}
+          />
+        }
+        titleTypographyProps={{
+          sx: {
+            fontSize: '1rem'
+          }
+        }}
+        subheaderTypographyProps={{
+          sx: {
+            color: 'rgba(255,255,255,0.4)'
+          }
+        }}
+      />
+      <CardContent sx={{ color: 'rgba(255,255,255,0.7)' }}>
+        <Typography variant='body1'>{review.content}</Typography>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default ReviewItem;
