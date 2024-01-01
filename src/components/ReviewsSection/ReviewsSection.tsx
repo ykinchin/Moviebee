d2bb4cd704a4309a79c1cd82dcc856ef0a08cb37
Yury Kinchin <yury.kinchyn@gmail.com>
2024-01-01 21:22:02 +0300

    feat: create component to display all reviews

---
+++ src/components/ReviewsSection/ReviewsSection.tsx
@@ -0,0 +1,27 @@
+import { FC } from 'react';
+import { IReview } from '../../shared/types';
+import { Box } from '@mui/material';
+import ReviewItem from './ReviewItem';
+
+interface ReviewsSectionProps {
+  reviews: IReview[];
+}
+
+const ReviewsSection: FC<ReviewsSectionProps> = ({ reviews }) => {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        gap: 4,
+        mb: 10
+      }}
+    >
+      {reviews.map((review) => (
+        <ReviewItem review={review} />
+      ))}
+    </Box>
+  );
+};
+
+export default ReviewsSection;
