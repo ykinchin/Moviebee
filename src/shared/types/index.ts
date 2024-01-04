21a746737304eb7edbefb43b6a7fca9dc89b2fc0
Yury Kinchin <yury.kinchyn@gmail.com>
2024-01-04 13:48:09 +0300

    feat: create person interface

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -119,3 +119,20 @@ export interface IReview {
   updated_at: string;
   url: string;
 }
+
+export interface IPerson {
+  adult: boolean;
+  also_known_as: string[];
+  biography: string;
+  birthday: string;
+  deathday: null | string;
+  gender: number;
+  homepage: null | string;
+  id: number;
+  imdb_id: string;
+  known_for_department: string;
+  name: string;
+  place_of_birth: string;
+  popularity: number;
+  profile_path: string;
+}
