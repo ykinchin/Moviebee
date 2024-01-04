35abea843b897ea248e9b20f5549cf8c115749d4
Yury Kinchin <yury.kinchyn@gmail.com>
2024-01-04 13:49:12 +0300

    feat: create credit route

--- src/appRouter/appRoutes.ts
+++ src/appRouter/appRoutes.ts
@@ -1,6 +1,7 @@
 import MainPage from '../pages/MainPage';
 import MoviePage from '../pages/MoviePage';
 import MoviesPage from '../pages/MoviesPage';
+import PersonPage from '../pages/PersonPage';
 import TVShowsPage from '../pages/TVShowsPage';
 import { PUBLIC_ROUTES } from '../shared/constants/paths';
 
@@ -13,7 +14,8 @@ export const publicRoutes: TRoute[] = [
   { path: PUBLIC_ROUTES.main, Component: MainPage },
   { path: PUBLIC_ROUTES.movies, Component: MoviesPage },
   { path: PUBLIC_ROUTES.tvShows, Component: TVShowsPage },
-  { path: PUBLIC_ROUTES.details, Component: MoviePage }
+  { path: PUBLIC_ROUTES.details, Component: MoviePage },
+  { path: PUBLIC_ROUTES.person, Component: PersonPage }
 ];
 
 // export const privateRoutes:TRoute[] = [
