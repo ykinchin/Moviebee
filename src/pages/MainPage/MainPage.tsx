a0f427dbbf3ed4b1ca0c2b367d64ddc38f5f641e
Yury Kinchin <yury.kinchyn@gmail.com>
2024-01-05 23:15:41 +0300

    feat: add isLoading state for data fetching

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -5,26 +5,36 @@ import { useMovie } from '../../hooks/useMovies';
 import { Box } from '@mui/material';
 
 const MainPage = () => {
-  const { data: popularTvs } = useTvShows('popular');
-  const { data: onTheAirTvs } = useTvShows('on_the_air');
-  const { data: popularMovies } = useMovie('popular');
-  const { data: upcomingMovies } = useMovie('upcoming');
+  const { isLoading: isLoadingPopularTVs, data: popularTvs } =
+    useTvShows('popular');
+  const { isLoading: isLoadingAirTVs, data: onTheAirTvs } =
+    useTvShows('on_the_air');
+  const { isLoading: isLoadingPopular, data: popularMovies } =
+    useMovie('popular');
+  const { isLoading: isLoadingUpcoming, data: upcomingMovies } =
+    useMovie('upcoming');
 
   return (
     <>
-      <HeroSection data={popularMovies} />
+      <HeroSection
+        data={popularMovies}
+        isLoading={isLoadingPopular}
+      />
       <Box px={15}>
         <Row
           title='Upcoming movies'
           data={upcomingMovies}
+          isLoading={isLoadingUpcoming}
         />
         <Row
           title='On the air TV-Shows'
           data={onTheAirTvs}
+          isLoading={isLoadingAirTVs}
         />
         <Row
           title='Popular TV-Shows'
           data={popularTvs}
+          isLoading={isLoadingPopularTVs}
         />
       </Box>
     </>
