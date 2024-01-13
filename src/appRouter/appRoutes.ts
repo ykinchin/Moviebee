2ce9a2356e786b50e53376b256a0318b20e99a66
Yury Kinchin <yury.kinchyn@gmail.com>
2024-01-13 22:49:45 +0300

    feat: add new user routes

--- src/appRouter/appRoutes.ts
+++ src/appRouter/appRoutes.ts
@@ -1,7 +1,12 @@
 import MainPage from '../pages/MainPage/MainPage';
 import MoviePage from '../pages/MoviePage/MoviePage';
 import PersonPage from '../pages/PersonPage/PersonPage';
-import { PUBLIC_ROUTES } from '../shared/constants/paths';
+import UserActors from '../pages/UserPage/pages/UserActors';
+import UserComments from '../pages/UserPage/pages/UserComments';
+import UserMovies from '../pages/UserPage/pages/UserMovies';
+import UserProfile from '../pages/UserPage/pages/UserProfile';
+import UserTVShows from '../pages/UserPage/pages/UserTVShows';
+import { APP_ROUTES } from '../shared/constants/paths';
 
 type TRoute = {
   path: string;
@@ -9,12 +14,13 @@ type TRoute = {
   Component: React.FC;
 };
 
-export const publicRoutes: TRoute[] = [
-  { path: PUBLIC_ROUTES.main, Component: MainPage },
-  { path: PUBLIC_ROUTES.details, Component: MoviePage },
-  { path: PUBLIC_ROUTES.person, Component: PersonPage }
+export const appRoutes: TRoute[] = [
+  { path: APP_ROUTES.main, Component: MainPage, isPrivate: false },
+  { path: APP_ROUTES.details, Component: MoviePage, isPrivate: false },
+  { path: APP_ROUTES.person, Component: PersonPage, isPrivate: false },
+  { path: APP_ROUTES.userProfile, Component: UserProfile, isPrivate: true },
+  { path: APP_ROUTES.userComments, Component: UserComments, isPrivate: true },
+  { path: APP_ROUTES.userActors, Component: UserActors, isPrivate: true },
+  { path: APP_ROUTES.userMovies, Component: UserMovies, isPrivate: true },
+  { path: APP_ROUTES.userTVShows, Component: UserTVShows, isPrivate: true }
 ];
-
-// export const privateRoutes:TRoute[] = [
-//   { path: PUBLIC_ROUTES.main, component: MainPage }
-// ];
