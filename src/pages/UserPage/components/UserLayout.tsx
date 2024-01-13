92cb98e1e96dd03b59019708c56dd00b84c9848e
Yury Kinchin <yury.kinchyn@gmail.com>
2024-01-13 22:49:14 +0300

    feat: add layout for user pages

---
+++ src/pages/UserPage/components/UserLayout.tsx
@@ -0,0 +1,30 @@
+import { FC } from 'react';
+import UserHeader from './UserHeader';
+import { Outlet } from 'react-router-dom';
+import { Box, Paper } from '@mui/material';
+
+const UserLayout: FC = () => {
+  return (
+    <Box
+      px={'20%'}
+      py={'1%'}
+    >
+      <Paper
+        square
+        variant='outlined'
+        sx={{
+          backgroundColor: 'inherit',
+          borderColor: 'rgba(255,255,255,0.4)',
+          color: 'white',
+          p: 1,
+          minHeight: '60vh'
+        }}
+      >
+        <UserHeader />
+        <Outlet />
+      </Paper>
+    </Box>
+  );
+};
+
+export default UserLayout;
