a3088054902317c697239757e1f4b449c1fe3e86
Yury Kinchin <yury.kinchyn@gmail.com>
2024-01-13 22:16:52 +0300

    feat: add array of user pages navigation

---
+++ src/shared/constants/userScreens.ts
@@ -0,0 +1,22 @@
+export const userScreens = [
+  {
+    title: 'Profile',
+    path: '/:user/profile'
+  },
+  {
+    title: 'My movies',
+    path: '/:user/movies'
+  },
+  {
+    title: 'My TV-shows',
+    path: '/:user/tv-shows'
+  },
+  {
+    title: 'Favorite actors',
+    path: '/:user/actors'
+  },
+  {
+    title: 'My comments',
+    path: '/:user/comments'
+  }
+];
