8e2d442d757e016b4088dcfa1405b36a19deb2e2
Yury Kinchin <yury.kinchyn@gmail.com>
2024-01-20 17:59:08 +0300

    feat: create basic markup

--- src/components/AuthForm/AuthForm.tsx
+++ src/components/AuthForm/AuthForm.tsx
@@ -1,9 +1,23 @@
 import { FC } from 'react';
 
-import { Box } from '@mui/material';
+import { Box, FormControl, Typography } from '@mui/material';
+
+import CustomInput from '../CustomInput/CustomInput';
 
 const AuthForm: FC = () => {
-  return <Box>AuthForm</Box>;
+  return (
+    <Box>
+      <Typography component="h1" fontSize={20} textAlign="center">
+        Registration
+      </Typography>
+      <FormControl
+        sx={{ display: 'flex', flexDirection: 'column', p: 3, gap: 3 }}
+      >
+        <CustomInput id="emailField" label="Email" />
+        <CustomInput id="passwordField" label="Password" />
+      </FormControl>
+    </Box>
+  );
 };
 
 export default AuthForm;
