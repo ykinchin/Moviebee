faea334283d19c96b68fb099529f99d38cc378dd
Yury Kinchin <yury.kinchyn@gmail.com>
2024-01-21 17:59:01 +0300

    feat: create basic markup

--- src/components/AuthForm/AuthForm.tsx
+++ src/components/AuthForm/AuthForm.tsx
@@ -1,21 +1,74 @@
 import { FC } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
-import { Box, FormControl, Typography } from '@mui/material';
+import { Box, Button, Stack, Tooltip, Typography } from '@mui/material';
 
+import useAuth from '../../hooks/useAuth';
 import CustomInput from '../CustomInput/CustomInput';
 
-const AuthForm: FC = () => {
+interface AuthFormProps {
+  formTitle: string;
+  buttonTitle: string;
+  text: string;
+  linkPath: '/registration' | '/login';
+}
+
+const AuthForm: FC<AuthFormProps> = ({
+  formTitle,
+  buttonTitle,
+  text,
+  linkPath,
+}) => {
+  const { guestSignIn } = useAuth();
+  const navigate = useNavigate();
+
+  const handleGuestLogin = async () => {
+    try {
+      await guestSignIn();
+      navigate('/', { replace: true });
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return (
-    <Box>
+    <Box
+      component="form"
+      sx={{
+        p: 5,
+        border: '1px solid rgba(255,255,255,0.3)',
+        display: 'flex',
+        flexDirection: 'column',
+      }}
+    >
       <Typography component="h1" fontSize={20} textAlign="center">
-        Registration
+        {formTitle}
       </Typography>
-      <FormControl
-        sx={{ display: 'flex', flexDirection: 'column', p: 3, gap: 3 }}
-      >
+      <Stack sx={{ py: 5, gap: 3 }}>
         <CustomInput id="emailField" label="Email" />
         <CustomInput id="passwordField" label="Password" />
-      </FormControl>
+      </Stack>
+      <Button>{buttonTitle}</Button>
+
+      <Stack sx={{ alignItems: 'center', mt: 5, width: '100%', gap: 1 }}>
+        <Typography>{text}</Typography>
+        <Link to={linkPath} style={{ width: '100%' }}>
+          <Button sx={{ width: '100%' }}>{formTitle}</Button>
+        </Link>
+        <Typography>or just</Typography>
+        <Tooltip
+          sx={{ width: '100%' }}
+          title={
+            <Typography sx={{ fontSize: 16, textAlign: 'center' }}>
+              Guest account is a demo account. You will have all the features,
+              but the movie collection will not be unique
+            </Typography>
+          }
+          placement="bottom"
+        >
+          <Button onClick={handleGuestLogin}>Sign in as Guest</Button>
+        </Tooltip>
+      </Stack>
     </Box>
   );
 };
