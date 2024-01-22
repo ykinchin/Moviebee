08d6c7b2f061bdf152d56c71dda16c9ab3763b18
Yury Kinchin <yury.kinchyn@gmail.com>
2024-01-22 17:19:49 +0300

    feat: create new login function

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -3,8 +3,20 @@ import { FC } from 'react';
 import Box from '@mui/material/Box/Box';
 
 import AuthForm from '../../components/AuthForm/AuthForm';
+import useAuth from '../../hooks/useAuth';
 
 const LoginPage: FC = () => {
+  const { signIn } = useAuth();
+  const signInHandler = async (email: string, password: string) => {
+    try {
+      console.log(email);
+
+      await signIn(email, password);
+    } catch (e) {
+      console.log(e.message);
+    }
+  };
+
   return (
     <Box
       sx={{
@@ -21,6 +33,7 @@ const LoginPage: FC = () => {
         text="If you still don't have an account?"
         linkTitle="Create Your Account"
         linkPath="/registration"
+        onClickHandler={signInHandler}
       />
     </Box>
   );
