f2e0af226e60743510fd6acfbb62d9bc1f544da0
Yury Kinchin <yury.kinchyn@gmail.com>
2024-01-22 17:22:34 +0300

    feat: add onChange props

--- src/components/CustomInput/CustomInput.tsx
+++ src/components/CustomInput/CustomInput.tsx
@@ -1,13 +1,14 @@
-import { FC } from 'react';
+import { ChangeEvent, FC } from 'react';
 
 import { TextField } from '@mui/material';
 
 interface CustomInputProps {
   id: string;
   label: string;
+  onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
 }
 
-const CustomInput: FC<CustomInputProps> = ({ id, label }) => {
+const CustomInput: FC<CustomInputProps> = ({ id, label, onChange }) => {
   return (
     <TextField
       id={id}
@@ -15,6 +16,7 @@ const CustomInput: FC<CustomInputProps> = ({ id, label }) => {
       variant="standard"
       InputLabelProps={{ style: { color: 'white' } }}
       InputProps={{ style: { color: 'white' } }}
+      onChange={onChange}
     />
   );
 };
