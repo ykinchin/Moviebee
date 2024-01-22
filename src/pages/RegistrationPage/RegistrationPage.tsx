630556fdea8292d4f3785c54b56dcbff9c63d2f2
Yury Kinchin <yury.kinchyn@gmail.com>
2024-01-22 17:18:40 +0300

    feat: remove depricated register function

--- src/pages/RegistrationPage/RegistrationPage.tsx
+++ src/pages/RegistrationPage/RegistrationPage.tsx
@@ -5,9 +5,6 @@ import Box from '@mui/material/Box/Box';
 import AuthForm from '../../components/AuthForm/AuthForm';
 
 const RegistrationPage: FC = () => {
-  // const handleRegister = (email: string, password: string) => {
-  //   createUserWithEmailAndPassword(auth, email, password);
-  // };
 
   return (
     <Box
